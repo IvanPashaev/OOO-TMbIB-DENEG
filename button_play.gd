a1cf0ec0602843f81aa20e26fbfaf23b3dc4f0f6
IvanPashaev <superivan2012p@gmail.com>
2026-02-01 19:27:00 +0300

    gun icon and project settings

--- button_play.gd
+++ button_play.gd
@@ -17,4 +17,5 @@ func _on_button_up() -> void:
 	var tween = get_tree().create_tween()
 	tween.tween_property($"../Play","scale", Vector2(4,4), 0.2)
 	tween.tween_property($"../LogoNt","skew", 0, 0.2)
+	#await get_tree().create_timer(5)
 	get_tree().change_scene_to_file("res://create_game.tscn")
---
+++ gun_cs.gd
@@ -0,0 +1,13 @@
+extends Sprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if $"../CheckBox".pressed:
+		var tween = create_tween()
+		tween.tween_property($".","rotation",151,1)
