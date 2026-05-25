ae58e134602919dc78ceb56a0410d56e940b62a6
IvanPashaev <superivan2012p@gmail.com>
2026-05-25 23:35:12 +0300

    work on game.tscn
    lol idk

--- scripts/button_play.gd
+++ scripts/button_play.gd
@@ -10,12 +10,16 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
-
+var test = true
 func _on_button_up() -> void:
 	print("play!")
 	
 	var tween = get_tree().create_tween()
 	tween.tween_property($"../Play","scale", Vector2(4,4), 0.2)
 	tween.tween_property($"../LogoNt","skew", 0, 0.2)
-	#await get_tree().create_timer(5)
-	get_tree().change_scene_to_file("res://create_game.tscn")
+	await get_tree().create_timer(1).timeout
+	
+	if test:
+		get_tree().change_scene_to_file("res://game.tscn")
+	else: get_tree().change_scene_to_file("res://create_game.tscn")
+	
---
+++ scripts/game_data.gd
@@ -0,0 +1,7 @@
+extends Node
+
+var game_data = {
+	"workers": 0,
+	"money": 0,
+	"name": 'ООО "тмыв денег"'
+}
